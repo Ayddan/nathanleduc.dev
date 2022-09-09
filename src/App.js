330e4f623b510b8ff78f7198d01f1a334b0ef0cf
Nathan Leduc <nathanleducmusique@gmail.com>
2022-09-09 17:25:22 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,153 @@
+import { useEffect, useState } from 'react';
+import './styles/css/style.css';
+import Lottie from 'react-lottie-player';
+import gsap from 'gsap'
+
+// Images
+import schertz from './assets/jpg/schertz.jpg';
+import todoApp from './assets/jpg/todo.jpg';
+import cefimVote from './assets/jpg/cefimvote.jpg';
+import tomleduc from './assets/jpg/tom-leduc.jpg';
+import flinbu from './assets/jpg/flinbu.jpg';
+
+// Components
+import Timeline from './components/Timeline';
+import Hero from './components/Hero';
+
+// Lottie
+import * as toogleThemeData from './assets/lottie/toogle-theme.json';
+import * as burgerButtonData from './assets/lottie/burger-button.json';
+import * as burgerButtonDarkData from './assets/lottie/burger-button-dark.json';
+
+function App() {
+  const [theme,setTheme] = useState('dark')
+  const [play,setPlay] = useState(false)
+  const [direction,setDirection] = useState(-1)
+
+  const [burgerDirection, setBurgerDirection] = useState(-1)
+  const [burgerPlay, setBurgerPlay] = useState(false)
+  const [burgerMenuOpen, setBurgerMenu] = useState(false)
+
+  const toogleTheme = () => {
+    if(theme === 'light') setTheme('dark')
+    else setTheme('light')
+
+    setPlay(true)
+    if(direction === 1) setDirection(-1)
+    else setDirection(1)
+    setBurgerPlay(true)
+  }
+
+  const toogleBurgerMenu = () => {
+    if(burgerMenuOpen){
+      gsap.to('.header-nav',{
+        x: 0,
+        duration: .3
+      })
+      setBurgerMenu(false)
+      setBurgerDirection(-1)
+      setBurgerPlay(true)
+    }else{
+      gsap.to('.header-nav',{
+        x:-100,
+        duration: .3
+      })
+      setBurgerMenu(true)
+      setBurgerDirection(1)
+      setBurgerPlay(true)
+    }
+  }
+
+  useEffect(()=>{
+    document.title= "Nathan Leduc | Dev Web"
+  },[])
+
+  
+  return (
+    <div className={`App ${theme}`}>
+      <header>
+        <div className='App-header container'>
+          <h2 className='site-title'>Nathan Leduc</h2>
+          <nav className='header-nav'>
+            <ul>
+              <li>
+                <a href='#projects'>Projets</a>
+              </li>
+              <li>
+                <a href='#journey'>Parcours</a>
+              </li>
+              <li>
+                <a href='mailto:nathanleduc.dev@gmail.com'>Contact</a>
+              </li>
+              <li>
+                <button className='toogle-theme' onClick={()=>{toogleTheme()}}>
+                  <Lottie
+                    loop={false}
+                    play={play}
+                    speed={2}
+                    direction={direction}
+                    onComplete={()=>setPlay(false)}
+                    animationData={toogleThemeData}
+                  />
+                </button>
+              </li>
+            </ul>
+          </nav>
+          <button className='burger-button' onClick={()=>{toogleBurgerMenu()}}>
+            <Lottie
+              loop={false}
+              play={burgerPlay}
+              speed={2}
+              direction={burgerDirection}
+              segments={[0,25]}
+              onComplete={()=>setBurgerPlay(false)}
+              animationData={theme === 'light' ? burgerButtonDarkData : burgerButtonData}
+            />
+          </button>
+        </div>
+      </header>
+      <Hero theme={theme}/>
+      <Timeline/>
+      <section className='container' id='projects'>
+        <div className='title-wrapper'>
+            <h2 className='hero-title title-with-decoration'>Mes projets</h2>
+            <hr className="separator"/>
+        </div>
+        <ul className='project-list'>
+          <li className='project-card'>
+            <a className='project-illustration' target='_blank' href='https://schertz.fr/'>
+              <img src={schertz}/>
+            </a>
+            <a className='project-name' target='_blank' href='https://schertz.fr/'>Schertz.fr</a>
+          </li>
+          <li className='project-card'>
+            <a className='project-illustration' target='_blank' href='https://todo-app-three-sable.vercel.app/'>
+              <img src={todoApp}/>
+            </a>
+            <a className='project-name' target='_blank' href='https://todo-app-three-sable.vercel.app/'>Todo app</a>
+          </li>
+          <li className='project-card'>
+            <a className='project-illustration' href='#'>
+              <img src={cefimVote}/>
+            </a>
+            <a className='project-name' href='#'>CefimVote (Non disponible)</a>
+          </li>
+          <li className='project-card'>
+            <a className='project-illustration' target='_blank' href='http://tomleduc.fr/'>
+              <img src={tomleduc}/>
+            </a>
+            <a className='project-name' target='_blank' href='http://tomleduc.fr/'>tomleduc.fr</a>
+          </li>
+          <li className='project-card'>
+            <a className='project-illustration' target='_blank' href='https://flinbu-oognd9w6y-ayddan.vercel.app/'>
+              <img src={flinbu}/>
+            </a>
+            <a className='project-name' target='_blank' href='https://flinbu-oognd9w6y-ayddan.vercel.app/'>Flinbu</a>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.js
@@ -0,0 +1,71 @@
+import React, { useEffect } from "react";
+import gsap from "gsap";
+
+import nathanImg from '../assets/jpg/nathan.jpg';
+import twitterDarkSvg from '../assets/svg/twitter-dark.svg';
+import linkedinDarkSvg from '../assets/svg/linkedin-dark.svg';
+import youtubeDarkSvg from '../assets/svg/youtube-dark.svg';
+import twitterLightSvg from '../assets/svg/twitter-light.svg';
+import linkedinLightSvg from '../assets/svg/linkedin-light.svg';
+import youtubeLightSvg from '../assets/svg/youtube-light.svg';
+
+const Hero = (props) => {
+    useEffect(()=>{
+        const entranceTimeline = gsap.timeline({paused: false})
+        entranceTimeline.to('#hero-separator',{
+            width: '115%',
+            duration: 1.2,
+            ease: 'Power2.easeInOut'
+        },1)
+        .to('.hero-title',{
+            y: 0,
+            duration: 1.7,
+            ease: 'Power2.easeInOut'
+        })
+        .to('.hero .sub-title',{
+            y: 0,
+            duration: 1.7,
+            ease: 'Power2.easeInOut'
+        },2.2)
+    })
+
+    return(
+        <section className='hero container'>
+            <div className='side-left'>
+            <div className='title-wrapper'>
+                <div className="text-box">
+                    <h1 className='hero-title'>Nathan Leduc</h1>
+                </div>
+                <hr id="hero-separator"/>
+                <div className="text-box">
+                    <span className='sub-title'>Developpeur web Fullstack</span>
+                </div>
+            </div>
+            <div className='hero-buttons flex-center'>
+                <a className='button-1' href='#projects'>Projects</a>
+                <a className='button-1' href='mailto:nathanleduc.dev@gmail.com'>Contact</a>
+            </div>
+            <ul className='social-list'>
+                <li>
+                    <a target='_blank' href='https://twitter.com/NathanLeduc5'>
+                        <img src={props.theme === 'light' ?  twitterLightSvg : twitterDarkSvg}/>
+                    </a>
+                </li>
+                <li>
+                    <a target='_blank' href='https://www.linkedin.com/in/nathan-leduc-349561146/'>
+                        <img src={props.theme === 'light' ?  linkedinLightSvg : linkedinDarkSvg}/>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <img src={props.theme === 'light' ?  youtubeLightSvg : youtubeDarkSvg}/>
+                    </a>
+                </li>
+            </ul>
+            </div>
+            <img className='hero-avatar' src={nathanImg} alt='Photo de Nathan'/>
+      </section>
+    )
+}
+
+export default Hero;
