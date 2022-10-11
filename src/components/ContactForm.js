0d47acd5f83474d8e4d25540326a318055f72cef
Ayddan <nathanleduc@hotmail.fr>
2022-10-11 12:38:22 +0200

    Adding contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -21,7 +21,7 @@ const ContactForm = () => {
             email: email.value,
             message: message.value,
         };
-        let response = await fetch("http://localhost:5000/contact", {
+        let response = await fetch("http://www.nathanleduc.dev:5000/contact", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json;charset=utf-8",
