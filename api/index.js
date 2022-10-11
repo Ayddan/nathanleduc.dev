eed870304e6ac567acfc1396383c390f2b098509
Ayddan <nathanleduc@hotmail.fr>
2022-10-11 13:38:20 +0200

    Fix domain problem for contact api

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors = require("cors");
 const nodemailer = require("nodemailer");
 
-app.use(cors());
+app.use(function (req, res, next) {
+    let origin = req.headers.origin;
+    res.header("Access-Control-Allow-Origin", req.headers.host.indexOf("localhost") > -1 ? "http://localhost:3000" : origin);
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(express.json());
 app.post('/api/contact', (req,res)=>{
     const contactEmail = nodemailer.createTransport({
